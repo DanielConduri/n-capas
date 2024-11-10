592df602df7db3771e58fb883ee6bf70b666c2d4
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-11-10 11:05:59 -0500

    server configuration 3

--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -19,6 +19,20 @@ router.get('/apiv3/data', (req, res) =>{
     })
 })
 
+router.get('/apiv3/prod', (req, res) =>{
+    res.json({
+        status: true,
+        message: "success",
+        body: [
+            {id: 1, name: "Prod 1"},
+            {id: 2, name: "Prod 2"},
+            {id: 3, name: "Prod 3"}
+            
+
+        ]
+    })
+})
+
 router.use('/apiv3/users', usersRoute);
 
 
